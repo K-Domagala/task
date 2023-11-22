a1f9ac53cb32be2dd762038ec395d07821866a3c
Krystian Domagala <krystian5250@gmail.com>
2023-11-22 21:39:52 +0000

    Setting up AJAX query

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,3 +1,16 @@
 $('#submit1').click(function() {
-    console.log($('#api1Input').val())
-})
+    $.ajax({
+        url: 'api.geonames.org/neighbours.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            country: $('#api1Input').val()
+        },
+        success: (result) => {
+            console.log(JSON.stringify(result))
+        },
+        error: () => {
+            console.log('something went wrong')
+        }
+    });
+});
