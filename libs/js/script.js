9115c2b5e2c428852c270954d005dc7c4ef1eb23
Krystian Domagala <krystian5250@gmail.com>
2023-11-22 21:57:05 +0000

    Added placeholder PHP

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,15 +1,16 @@
 $('#submit1').click(function() {
     $.ajax({
-        url: 'api.geonames.org/neighbours.php',
+        url: 'libs/php/neighbours.php',
         type: 'POST',
         dataType: 'json',
         data: {
             country: $('#api1Input').val()
         },
         success: (result) => {
-            console.log(JSON.stringify(result))
+            console.log(result['data'][0])
         },
-        error: () => {
+        error: (error) => {
+            console.log(error);
             console.log('something went wrong')
         }
     });
