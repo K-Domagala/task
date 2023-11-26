b9cac604aae873b577955f074a779f94a5e20952
Krystian Domagala <krystian5250@gmail.com>
2023-11-26 15:16:16 +0000

    Added functionality for 1st API

--- libs/js/script.js
+++ libs/js/script.js
@@ -7,11 +7,43 @@ $('#submit1').click(function() {
             country: $('#api1Input').val()
         },
         success: (result) => {
-            console.log(result['data'][0])
+            let resultString = '';
+            resultString += result['data'][0]['countryName']
+            for(let i = 1; i < result['data'].length; i++){
+                resultString += ', ' + result['data'][i]['countryName'];
+            }
+            $('#result').html(resultString)
         },
         error: (error) => {
             console.log(error);
             console.log('something went wrong')
+            $('#result').html(error.responseText)
         }
     });
+});
+
+$(window).on('load', function () {
+    if ($('#preloader').length) {
+        $('#preloader').delay(1000).fadeOut('slow', function () {
+            $(this).remove();
+        });
+    }
+    $.ajax({
+        url: 'libs/php/countries.php',
+        type: 'GET',
+        dataType: 'json',
+        success: (result) => {
+            let response = ''
+            console.log(result['data'][0]['countryCode'])
+            for(let i = 0; i < result['data'].length; i++){
+                response += '<option value=' + result['data'][i]['countryCode'] + '>' + result['data'][i]['countryName'] + '</option>'
+            }
+            $('#api1Input').html(response)
+        },
+        error: (error) => {
+            console.log(error);
+            console.log('something else went wrong')
+            $('#result').html(error.responseText)
+        }
+    })
 });
